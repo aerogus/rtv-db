2798995eb6466785566d67e677ca834e0a75798a
Guillaume Seznec <guillaume@seznec.fr>
2025-03-15 10:38:43 +0100

    logo + tool

---
+++ src/mklogos.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+declare INPUT=$1
+declare CCC=${INPUT:0:3}
+
+echo "Création logos"
+
+echo "$INPUT -> $CCC.png"
+magick -background none "$INPUT" -resize 300x95 "$CCC.png"
+
+echo "$INPUT -> $CCC.jpg"
+magick "$INPUT" -resize 90x80 -gravity center -extent 100x80 "$CCC.jpg"
+
+echo "FIN"
